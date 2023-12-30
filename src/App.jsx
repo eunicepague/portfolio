4706651db8cb9a7bb72401be7e372daac0d86eeb
eunicepague <eunicegracepague@gmail.com>
2023-12-30 18:16:06 +0800

    fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import Landing from './components/Hero/Landing';
 import Hero from './components/Hero/Hero';
 import About from './components/About/About';
 import Tech from './components/Techstack/Tech';
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,20 +1,57 @@
 // import React from 'react';
+import { useState, useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import Me from './../../assets/eunicepague.png';
+
+import Crown from './../../assets/crown.png';
 
 import './Hero.css';
 
 const Hero = () => {
+  const [isHovered, setIsHovered] = useState(false);
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+    document
+      .getElementById('landing-content')
+      .addEventListener('mousemove', handleMouseMove);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+    document
+      .getElementById('landing-content')
+      .removeEventListener('mousemove', handleMouseMove);
+  };
+
+  const handleMouseMove = (e) => {
+    const crownContent = document.getElementById('landing-content');
+    const rect = crownContent.getBoundingClientRect();
+    setMousePosition({
+      x: e.clientX - rect.left - 88, // subtract half the width of the image
+      y: e.clientY - rect.top - 160, // subtract half the height of the image
+    });
+  };
+
+  useEffect(() => {
+    return () => {
+      document
+        .getElementById('landing-content')
+        .removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
   return (
     <Container fluid className="hero-container">
       <section className="hero-content">
         <Row>
           <Col className="hero-left" sm={12} md={6} lg={6}>
             <div className="hero-intro">
-              <h1>FRONTEND DEVELOPER</h1>
+              <h4>FRONTEND DEVELOPER</h4>
               <h1>
-                Hello,
-                <span style={{ color: '#d73d57' }}>I'm Euncice Pague</span>
+                Hello,{' '}
+                <span style={{ color: '#d73d57' }}>I'm Eunice Pague!</span>
               </h1>
               <p className="mt-3">
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto,
@@ -32,6 +69,30 @@ const Hero = () => {
             </div>
           </Col>
           <Col className="hero-right" sm={12} md={6} lg={6}>
+            <div id="landing-content" onMouseEnter={handleMouseEnter}>
+              {isHovered && (
+                <img
+                  className="landingCrown d-none d-md-flex"
+                  src={Crown}
+                  style={{
+                    position: 'absolute',
+                    left: mousePosition.x + 'px',
+                    top: mousePosition.y + 'px',
+                  }}
+                />
+              )}
+              <div
+                style={{
+                  position: 'absolute',
+                  top: 0,
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                }}
+                onMouseLeave={handleMouseLeave}
+              />
+            </div>
+
             <div className="hero-img-container">
               <img src={Me} />
             </div>
